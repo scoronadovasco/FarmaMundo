c9118a613267ce6610cc23f127dc3e517e33dc3e
scoronadovasco <scoronadovascogmail.com>
2024-01-18 21:52:41 -0500

    creating relation and created loadDatabase

--- DAL/Product.cs
+++ DAL/Product.cs
@@ -10,7 +10,7 @@ namespace FarmaMundo.DAL
         public int Idproduct { get; set; }
         
         [Required]
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public string? Description { get; set; }
         public double? Price { get; set; }
         public DateTime CreateDate { get; set; }
