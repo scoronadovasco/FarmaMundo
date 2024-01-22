44d4d90b9510c42e4ad768b309aac433e8606075
scoronadovasco <scoronadovascogmail.com>
2024-01-22 16:33:59 -0500

    aplicando configuracion a loaddatabase

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using FarmaMundo.DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,9 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DatabaseContext>(p =>
-{ 
-p.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+{
+    p.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
 
@@ -22,10 +26,34 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+using (var ambiente = app.Services.CreateScope())
+{
+    var services = ambiente.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DatabaseContext>();
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await LoadDatabase.CargarData(context, userManager, roleManager);
+    }
+    catch (Exception e)
+    {
+
+        var loggin = services.GetRequiredService<ILogger<Program>>();
+        loggin.LogError(e, "ocurrio un error");
+    }
+
+}
+
 
 app.Run();
